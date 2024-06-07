0ad424d24d29600791de43972caf37816ac9bb1a
kio ino <tg:@kekekio>
2024-06-07 17:32:43 +0300

    structure has been changed

--- install.bash
+++ install.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 # \brief install all the configs using symlinks, points to here
 # установить все конфиги с помощью ссылок сюда
-ln -s ./.tmux.conf ~/.tmux.conf
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+FILE_NAME=".tmux.conf"
+ln -s "$SCRIPT_DIR/tmux/$FILE_NAME" "$HOME/$FILE_NAME"
 tmux source-file ~/.tmux.conf
+
+FILE_NAME="init.lua"
+ln -s "$SCRIPT_DIR/neovim/$FILE_NAME" "$HOME/.config/nvim/$FILE_NAME"
+FILE_NAME="nvimrc.vim"
+ln -s "$SCRIPT_DIR/neovim/$FILE_NAME" "$HOME/.config/nvim/$FILE_NAME"
---
+++ neovim/nvimrc.vim
@@ -0,0 +1,7 @@
+set mouse=
+
+xnoremap p pgvy
+
+"leader key is comma ","
+let mapleader = ","
+let g:mapleader = ","
