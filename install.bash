498f8d8bbee0cdc501c0bd873c8dbaef09570d63
kio ino <tg:@kekekio>
2024-06-24 18:13:26 +0300

    fix

--- install.bash
+++ install.bash
@@ -2,13 +2,21 @@
 # \brief install all the configs using symlinks, points to here
 # установить все конфиги с помощью ссылок сюда
 
+CONFIG_NAME="kio_setup"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-FILE_NAME=".tmux.conf"
-ln -s "$SCRIPT_DIR/tmux/$FILE_NAME" "$HOME/$FILE_NAME"
-tmux source-file ~/.tmux.conf
+mkdir -p "$HOME/mnt/"{build_server,bmc,ssh}
+mkdir -p "$HOME/mnt/ssh/"{0..9}
 
-FILE_NAME="init.lua"
-ln -s "$SCRIPT_DIR/neovim/$FILE_NAME" "$HOME/.config/nvim/$FILE_NAME"
-FILE_NAME="nvimrc.vim"
-ln -s "$SCRIPT_DIR/neovim/$FILE_NAME" "$HOME/.config/nvim/$FILE_NAME"
+FILE_NAME=".tmux.conf"
+ln -sf "$SCRIPT_DIR/tmux/$FILE_NAME" "$HOME/$FILE_NAME"
+tmux source-file "$HOME/.tmux.conf"
+
+ln -s "$SCRIPT_DIR/nvim/" "$HOME/.config/"
+
+ln -s "$SCRIPT_DIR/$CONFIG_NAME/" "$HOME/.config/"
+
+FILE_NAME=".bashrc"
+ln -sf "$SCRIPT_DIR/$CONFIG_NAME/$FILE_NAME" "$HOME/$FILE_NAME"
+
---
+++ kio_setup/setup.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+# \brief setup all env vars
+
+mkdir -p "$HOME/mnt{build_server,bmc,ssh}"
+mkdir -p "$HOME/mnt/ssh{0..9}"
+export MOUNT_FUSE_DIR="$HOME/mnt"
+CONFIG_PATH="$HOME/.config/kio_setup/servers.json"
+SERVERS_JSON=$(cat "$CONFIG_PATH")
+
+
+export ALICE_SERVER_IP=$(jq ".alice_server.ip" -r <<< $SERVERS_JSON)
+export ALICE_SERVER_USERNAME=$(jq ".alice_server.username" -r <<< $SERVERS_JSON)
+function connect_alice() {
+	ssh $ALICE_SERVER_USERNAME@$ALICE_SERVER -t tmux attach-session
+}
+function mount_alice() {
+	export ALICE_MOUNT_POINT="$MOUNT_FUSE_DIR/ssh/0"
+	sshfs -o allow_other $ALICE_SERVER_USERNAME@$ALICE_SERVER:/ $ALICE_MOUNT_POINT
+}
+function umount_alice() {
+	sudo umount -l $ALICE_MOUNT_POINT
+}
+export -f connect_alice
+export -f mount_alice
+export -f umount_alice
+
+
+export BUILDER_SERVER_IP=$(jq ".build_server.ip" -r <<< $SERVERS_JSON)
+function connect_builder() {
+	ssh $BUILDER_SERVER_IP -t tmux attach-session
+}
+function mount_builder() {
+	sshfs -o allow_other $BUILDER_SERVER_IP:/ $MOUNT_FUSE_DIR/build_server/
+}
+function umount_builder() {
+	sudo umount -l $MOUNT_FUSE_DIR/build_server/
+}
+export -f connect_builder
+export -f mount_builder
+export -f umount_builder
+
+
+function umount_bmc() {
+	sudo umount -l "$HOME/mnt/bmc"
+}
+export -f connect_builder
+
+#source $fullfile
+
+
